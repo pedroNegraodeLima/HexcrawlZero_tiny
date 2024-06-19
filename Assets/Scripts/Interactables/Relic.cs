ec54f370abc7d73111c5f50967101b8571356d62
Pedro Lima <pedro.negraodelima@gmail.com>
2024-06-19 17:20:55 -0300

    interactable placeholders positioning

--- Assets/Scripts/Interactables/Relic.cs
+++ Assets/Scripts/Interactables/Relic.cs
@@ -6,10 +6,24 @@ public class Relic : MonoBehaviour, IInteractable
 {
     [SerializeField] private string prompt;
     public string InteractorPrompt => prompt;
+
+    public Dialogue dialogue;
     public bool Interact(PlayerPickUp interactor)
     {
         Debug.Log("This is a Relic!");
+
+        TriggerDialogue();
+
         return true;
     }
 
+    private void TriggerDialogue()
+    {
+        var dialogueManager = DialogueManager.Get();
+
+        if (!dialogueManager.canDialogue) return;
+
+        dialogueManager.StartDialogue(dialogue);
+        
+    }
 }
