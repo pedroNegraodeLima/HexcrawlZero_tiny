0c9b270c6f5abcfca3704ae236089a81ebe7a239
Pedro Lima <pedro.negraodelima@gmail.com>
2024-07-25 10:03:25 -0300

    UI stuff and added fade in to game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject quitMenu;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         quitMenu.SetActive(false);
